fa511751fabab5ac56a749c4070c086f0b2b2a5b
16192005 <16192005@L815WD150036.edu.cbe.ab.ca>
2022-03-17 15:22:00 -0600

    updating

--- CS10/src/Assingments/MyInitials.java
+++ CS10/src/Assingments/MyInitials.java
@@ -17,7 +17,24 @@ public class MyInitials {
 
 	public static void main(String[] args) 
 	{
-		System.out.print("--------      ");	
+		System.out.println("--------\t|\t|");
+		System.out.println("|\t\t|\t|");
+		System.out.println("|\t\t|\t|");
+		System.out.println("|\t\t|\t|");
+		System.out.println("|\t\t---------");
+		System.out.println("|\t\t|\t|");
+		System.out.println("|\t\t|\t|");
+		System.out.println("|\t\t|\t|");
+		System.out.println("--------\t|\t|");
 	}
+	
+	/* Screen Dump
+---------
+|
+
+
+
+	 
+	 */	
 
 }
