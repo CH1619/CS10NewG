545bd53e28214abad620dbce2b5c690474f725ac
16192005 <16192005@L815WD220034.edu.cbe.ab.ca>
2022-04-01 11:32:11 -0600

    Updating

---
+++ CS10/src/Assingments/Assingment05.java
@@ -0,0 +1,29 @@
+/*
+
+Program: Assingment05.java          Date: Apr 1 
+
+
+Author: Charlie Houston
+School: CHHS
+Course: Computer Science 10
+ 
+
+*/
+
+package Assingments;
+
+public class Assingment05 
+{
+
+	public static void main(String[] args) 
+	{
+	int x= 79 + 3 * (4 + 82 - 68) - 7 +19;
+	int y= (179 + 21 + 10) / 7 + 181;
+	int z= 10389 * 56 * 11 + 2246;
+
+	System.out.println("10389 * 56 * 11 + 2246= " x );
+	System.out.print(x);
+	
+	}
+
+}
