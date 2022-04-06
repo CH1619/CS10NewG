8c01f6288424baab617c16676bacf7960565d167
16192005 <16192005@L815WD220034.edu.cbe.ab.ca>
2022-04-06 13:47:29 -0600

    Updating

---
+++ CS10/src/Assingments/ChangeApplication.java
@@ -0,0 +1,42 @@
+/*
+
+Program: ChangeApplication.java          Date: Apr 6 2022
+
+
+Author: Charlie Houston 
+School: CHHS
+Course: Computer Science 10
+ 
+
+*/
+package Assingments;
+
+import java.util.Scanner;
+
+public class ChangeApplication 
+{
+
+	public static void main(String[] args) 
+	{
+	Scanner userinput = new Scanner(System.in);
+	
+	System.out.println("Enter the ammount of change you have: ");
+	
+	int change = userinput.nextInt();
+	
+	int quarters = change / 25;
+	
+	int dimes = (change % 25)/10;
+	
+	int nickels = ((change % 25)%10)/5;
+	
+	System.out.println("The ammount of change you have is: " + change);
+	
+	System.out.println("Quarters: " + quarters);
+	
+	System.out.println("Dimes: " + dimes);
+	
+	System.out.println("Nickels: " + nickels);
+	}
+
+}
