eac8c7ec6bc6d11df09af20063ee5a442a2572b5
16192005 <16192005@L815WD220034.edu.cbe.ab.ca>
2022-04-11 13:32:14 -0600

    updating

--- CS10/src/CS10.java
+++ CS10/src/CS10.java
@@ -5,7 +5,16 @@ public class CS10
 	public static void main(String[] args) 
 	{
 		System.out.print("Charlie Houston ");
-		System.out.print("CHHS");
+		System.out.print("CHHS \n");
+	
+		System.out.println("|       |  --------");
+		System.out.println("|*     *|  |      |");
+		System.out.println("| *   * |  |      |");
+		System.out.println("|  * *  |  |      |");
+		System.out.println("|   *   |  |-------");
+		System.out.println("|       |  |");
+		System.out.println("|       |  |");
+	
 	}
 
 }
