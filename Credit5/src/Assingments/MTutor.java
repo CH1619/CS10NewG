3c7c6381aab4744f55532b699d214993ad008381
16192005 <16192005@L815WD220034.edu.cbe.ab.ca>
2022-04-26 15:24:58 -0600

    Updating

--- Credit5/src/Assingments/MTutor.java
+++ Credit5/src/Assingments/MTutor.java
@@ -12,88 +12,87 @@ Course: Computer Science 10
 
 package Assingments;
 
-import java.util.Scanner;
+import java.util.Scanner;//import scanner
 
 public class MTutor 
 {
 
 	public static void main(String[] args) 
 	{
-	Scanner userinput = new Scanner(System.in);
+		Scanner userinput = new Scanner(System.in);//create scanner
 	
-	Scanner Qanswer = new Scanner(System.in);
+		
+		int randomnum1 = 1 + (int) (Math.random() * 10);//creating random numbers
 	
-	int randomnum1 = 1 + (int) (Math.random() * 10);
+		int randomnum2 = 1 + (int) (Math.random() * 10);
 	
-	int randomnum2 = 1 + (int) (Math.random() * 10);
+		System.out.println("Select an operator by typing the associated number: ");//prompt for operator
 	
-	System.out.println("Select an operator by typing the associated number: ");
+		System.out.println("1 : Addition");//output options 
+		System.out.println("2 : Subtraction");
+		System.out.println("3 : Division");
+		System.out.println("4 : Multiplication");
 	
-	System.out.println("1 : Addition");
-	System.out.println("2 : Subtraction");
-	System.out.println("3 : Division");
-	System.out.println("4 : Multiplication");
+		int operator = userinput.nextInt();//record operator
 	
-	int operator = userinput.nextInt();
+		switch (operator)//creating switch statement
+		{
+		case 1://Addition 
+			int Canswer = randomnum1 + randomnum2;//create correct answer
+			System.out.println("What is " + randomnum1 + "+" + randomnum2 + "?");//prompt for answer
+			int answer = userinput.nextInt();//record answer
+			if (answer == Canswer)//checking answer, create if statement 
+			{
+				System.out.println("You got it right! Congradulations!!");//output message 
+			}
+			else//creating else statement if wrong
+			{
+				System.out.println("you were incorrect. You'll get it next time!");//output message
+			}
+			break;//break case
 	
-	switch (operator)
-	{
-	case 1: 
-	int Canswer = randomnum1 + randomnum2;
-	System.out.println("What is " + randomnum1 + "+" + randomnum2 + "?");
-	int answer = Qanswer.nextInt();
-	if (answer == Canswer)
-	{
-	System.out.println("You got it right! Congradulations!!");
-	}
-	else
-	{
-	System.out.println("you were incorrect. You'll get it next time!");
-	}
-	break;
+		case 2://Subtraction
+			int Canswer2 = randomnum1 - randomnum2;//create correct answer
+			System.out.println("What is " + randomnum1 + "-" + randomnum2 + "?");//prompt for answer
+			int answer2 = userinput.nextInt();//record answer
+			if (answer2 == Canswer2)//checking answer, create if statement
+			{
+				System.out.println("You got it right! Congradulations!!");//output message 
+			}
+			else//creating else statement if wrong
+			{
+				System.out.println("you were incorrect. You'll get it next time!");//output message 
+			}
+			break;//break case
 	
-	case 2:
-	int Canswer2 = randomnum1 - randomnum2;
-	System.out.println("What is " + randomnum1 + "-" + randomnum2 + "?");
-	int answer2 = Qanswer.nextInt();
-	if (answer2 == Canswer2)
-	{
-	System.out.println("You got it right! Congradulations!!");
-	}
-	else
-	{
-	System.out.println("you were incorrect. You'll get it next time!");
-	}
-	break;
+		case 3://Division
+			int Canswer3 = randomnum1/randomnum2;//create correct answer
+			System.out.println("What is " + randomnum1 + "/" + randomnum2 + "?");//prompt for answer
+			int answer3 = userinput.nextInt();//record answer
+			if (answer3 == Canswer3)//checking answer, create if statement
+			{
+				System.out.println("You got it right! Congradulations!!");//output message 
+			}
+			else//creating else statement if wrong
+			{
+				System.out.println("you were incorrect. You'll get it next time!");//output message 
+			}
+			break;//break case
 	
-	case 3:
-	int Canswer3 = randomnum1/randomnum2;
-	System.out.println("What is " + randomnum1 + "/" + randomnum2 + "?");
-	int answer3 = Qanswer.nextInt();
-	if (answer3 == Canswer3)
-	{
-	System.out.println("You got it right! Congradulations!!");
-	}
-	else
-	{
-	System.out.println("you were incorrect. You'll get it next time!");
-	}
-	break;
-	
-	case 4:
-	int Canswer4 = randomnum1*randomnum2;
-	System.out.println("What is " + randomnum1 + "*" + randomnum2 + "?");
-	int answer4 = Qanswer.nextInt();
-	if (answer4 == Canswer4)
-	{
-	System.out.println("You got it right! Congratulations!!");
-	}
-	else
-	{
-	System.out.println("You were incorrect. You'll get it next time!");
-	}
-	break;
-	}
+		case 4://Multiplication
+			int Canswer4 = randomnum1*randomnum2;//create correct answer
+			System.out.println("What is " + randomnum1 + "*" + randomnum2 + "?");//prompt for answer
+			int answer4 = userinput.nextInt();//record answer
+			if (answer4 == Canswer4)//checking answer, create if statement
+			{
+				System.out.println("You got it right! Congratulations!!");//output message 
+			}
+			else//creating else statement if wrong
+			{
+				System.out.println("You were incorrect. You'll get it next time!");//output message 
+			}
+			break;//break case
+		}//end switch statement
 	
 	}
 
