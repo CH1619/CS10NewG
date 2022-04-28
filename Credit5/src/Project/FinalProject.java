b0d803819482f8bd23e900c944d0b0fcfc241acc
16192005 <16192005@L815WD220034.edu.cbe.ab.ca>
2022-04-28 15:10:29 -0600

    Updating

--- Credit5/src/Project/FinalProject.java
+++ Credit5/src/Project/FinalProject.java
@@ -27,47 +27,47 @@ public class FinalProject
 		
 		int size = userinput.nextInt();//record size
 		
-		double price = 0.00;
+		double price = 0.00;//creating price
 		
-		while(size != -1)
+		while(size != -1)//creating while loop is size is chosen
 		{
 		
-		if (size == 1)
+		if (size == 1)//create if statement
 		{
-			price = 3.00;
+			price = 3.00;//assign price
 
 		}
-		else if (size == 2)
+		else if (size == 2)//create else if statement
 		{
-			price = 5.00;
+			price = 5.00;//assign price
 		}
-		else 
+		else//else statement
 		{
-			price = 7.00;
-		}
-		
-		System.out.println("Your price is: " + price + "0$");
-		
-		System.out.println("Would you like to add butter for $1?");
-		
-		System.out.println("1: no butter 2: add butter");
-		
-		int butter = userinput.nextInt();
-		
-		if (butter == 2)
-		{
-		Double Nprice = price + 1.00;
-		
-		System.out.println("Your price is: " + Nprice + "0$");
-		
+			price = 7.00;//assign price
 		}
 		
+		System.out.println("Your price is: " + price + "0$");//display initial price
 		
-		System.out.println("Please enter another size or -1 to quit");
+		System.out.println("Would you like to add butter for $1?");//prompt for butter
 		
-		size = userinput.nextInt();
-		} 
-		System.out.println("Transaction complete, enjoy your movie!");
+		System.out.println("1: no butter 2: add butter");//display choice
+		
+		int butter = userinput.nextInt();//record answer
+		
+		if (butter == 2)//create if statement for yes
+		{
+		Double Nprice = price + 1.00;//calculating new price
+		
+		System.out.println("Your price is: " + Nprice + "0$");//display new price
+		}
+		
+		
+		System.out.println("Please enter another size or -1 to quit");//prompt for new size
+		
+		size = userinput.nextInt();//record size
+		}//loop   
+		
+		System.out.println("Transaction complete, enjoy your movie!");//display exit message
 		
 	}
 
