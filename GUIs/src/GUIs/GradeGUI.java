215cc1bd15bc797acd35f195cf888088df8b32a8
16192005 <16192005@L815WD220034.edu.cbe.ab.ca>
2022-05-03 14:44:01 -0600

    Updating

---
+++ GUIs/src/GUIs/GradeGUI.java
@@ -0,0 +1,43 @@
+package GUIs;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class GradeGUI {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GradeGUI window = new GradeGUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public GradeGUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
